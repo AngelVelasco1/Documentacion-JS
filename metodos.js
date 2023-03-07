7736100d2cbf1acf075f765356a7ea2e775ae321
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-07 13:03:07 -0500

    feat: :sparkles: Metodo substring
    Este metodo nos permite devolver un subconjunto de un objeto string. Estamos devolviendo en palabra 2 los strings que hacen referencia desde la posicion 0 a la 7.

--- metodos.js
+++ metodos.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 let palabra =  "Ejemplo Metodo slice";
 let palabra2 = palabra.slice(7, 20);
 
+=======
+let palabra =  "Ejemplo Metodo substring"
+let palabra2 = palabra.substring(0, 7);
+>>>>>>> 0a7ebe6 (feat: :sparkles: Metodo substring)
 console.log(palabra2);
