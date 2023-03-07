e157847b122cfd7cc371563ca9df90d11d2e5094
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-07 13:10:36 -0500

    feat: :sparkles: Metodo charAT
    Con este metodo estamos mostrando el elemento 3 que hace referencia a m.  Esto lo hacemos especificando la posicion del char que queremos mostrar, comenzando en 0 para el primer char.

--- metodos.js
+++ metodos.js
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-let palabra =  "Ejemplo Metodo slice";
-let palabra2 = palabra.slice(7, 20);
-
-=======
-let palabra =  "Ejemplo Metodo substring"
-let palabra2 = palabra.substring(0, 7);
->>>>>>> 0a7ebe6 (feat: :sparkles: Metodo substring)
-console.log(palabra2);
-=======
 let palabra =  "Ejemplo Metodo repeat "
 console.log(palabra.repeat(3));
->>>>>>> 689d6be (feat: :sparkles: Metodo repeat)
