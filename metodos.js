87de28ac9e19406e95880d953e29982700e097bc
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 12:50:26 -0500

    feat: :sparkles: Metodo de comparacion !=
    Nos permite comparar si dos valores son diferentes. En caso de serlo retornara true, de lo contrario si son iguales retornara false. "x" y "y" no son diferentes por lo que su resultado es false.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
 let x = true;
 let y = true;
 
-let result = x == y; 
-console.log(result);
+let result = x != y; 
+console.log("Los valores no son diferentes", result);
 
 let ejemplo2 = true;
 let ejemplo3 = false;
 
-let result2 = ejemplo2 == ejemplo3;
-console.log(result2);
+let result2 = ejemplo2 != ejemplo3;
+console.log("Los valores son diferentes", result2);
