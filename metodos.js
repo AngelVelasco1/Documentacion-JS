ebd9ec78cc392c41f4d6b08c00b3382836521807
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 12:53:34 -0500

    feat: :sparkles: Operador de comparacion ===
    Verifica si el valor y el tipo de dato son iguales. En el primer ejemplo "x" y "y" son iguales tanto en valor como en tipo, por lo que sera true.  En el segundo ambos tienen el mismo valor pero son de distinto tipo por lo que sera false.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
-let x = true;
-let y = true;
+let x = 25;
+let y = 25;
 
-let result = x != y; 
-console.log("Los valores no son diferentes", result);
+let result = x === y; 
+console.log("Los valores son iguales en valor y tipo", result);
 
-let ejemplo2 = true;
-let ejemplo3 = false;
+let ejemplo2 = "5";
+let ejemplo3 = 5;
 
-let result2 = ejemplo2 != ejemplo3;
-console.log("Los valores son diferentes", result2);
+let result2 = ejemplo2 === ejemplo3;
+console.log("Los valores son iguales en valor pero diferentes en tipo", result2);
