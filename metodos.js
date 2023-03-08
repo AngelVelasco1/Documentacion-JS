281aff13f22dbb247fb8e8eb91af0e1fd0980c55
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 13:18:07 -0500

    feat: :sparkles: Operador de comparacion >= (Mayor o igual)
    Nos permite comparar si un valor es mayor o igual que otro. Solo necesita o ser mayor o igual para retornar true. En caso de ser menor retornar false. En el primer ejemplo "x" no es mayor pero si igual a "y" por lo que retornara true.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
-let x = "Hola amigos";
-let y = "Hola";
+let x = 20;
+let y = 20;
 
-let result = x > y; 
-console.log("El primer string es mayor al segundo", result);
+let result = x >= y; 
+console.log("El primer valor no es mayor pero si igual", result);
 
-let ejemplo2 = "Gato";
-let ejemplo3 = "Perro";
+let ejemplo2 = 25;
+let ejemplo3 = 20;
 
-let result2 = ejemplo2 > ejemplo3;
-console.log("El segundo string es mayor al primero", result2);
+let result2 = ejemplo2 >= ejemplo3;
+console.log("El segundo valor es mayor al primero", result2);
