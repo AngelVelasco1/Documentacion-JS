34151a5dfcb5e360b26c45fb5f75fee268d20bb0
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 13:24:35 -0500

    feat: :sparkles: Operador de comparacion <= (Menor o igual que)
    Compara si el primer valor es menor o igual al segundo. Con cumplir tan solo una condicion sera true. En el primer ejemplo "x" no es menor a "y" pero si igual, retornando true. En el segundo ejemplo el primer valor no es ni menor ni igual, retornando false.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
-let x = 15;
+let x = 20;
 let y = 20;
 
-let result = x < y; 
-console.log("El primer valor si es menor al segundo", result);
+let result = x <= y; 
+console.log("El primer valor no es menor pero si igual al segundo", result);
 
-let ejemplo2 = 25;
+let ejemplo2 = 250;
 let ejemplo3 = 20;
 
-let result2 = ejemplo2 < ejemplo3;
-console.log("El primer valor no es menor al seguno", result2);
+let result2 = ejemplo2 <= ejemplo3;
+console.log("El primer valor no es menor ni igual al segundo", result2);
