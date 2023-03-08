aa1ba382e1f21251f3b8d745e26b5e69623b077f
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 13:04:02 -0500

    feat: :sparkles: Operador de comparacion !== (Estrictamente diferente)
    Nos permite comprobar si dos valores son diferentes tanto en valor como en tipo. En caso de ser asi retornara true. De lo contrario sera false. En el primer ejemplo "x" y "y" son iguales tanto en valor como en tipo, por lo que sera false. Mientras que el segundo ejemplo son iguales en valor pero diferentes en tipo, por lo que retornara true.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
 let x = 25;
 let y = 25;
 
-let result = x === y; 
+let result = x !== y; 
 console.log("Los valores son iguales en valor y tipo", result);
 
 let ejemplo2 = "5";
 let ejemplo3 = 5;
 
-let result2 = ejemplo2 === ejemplo3;
+let result2 = ejemplo2 !== ejemplo3;
 console.log("Los valores son iguales en valor pero diferentes en tipo", result2);
