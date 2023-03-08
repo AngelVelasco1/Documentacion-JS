86b89d2e3a4841b5fa9be1bb04af0a06e89731dc
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 12:28:43 -0500

    feat: :zap: Operador or
    Ambos valores deben ser verdaderos para que nos retorne true. De lo contrario sera false

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
-let ejemplo1 = true && true;
+let ejemplo1 = true || true;
 console.log(ejemplo1);
 
-let ejemplo2 = true && false;
+let ejemplo2 = true || false;
 console.log(ejemplo2);
 
-let ejemplo3 = false && false;
+let ejemplo3 = false || false;
 console.log(ejemplo3);
 
-let ejemplo4 = false && true;
+let ejemplo4 = false || true;
 console.log(ejemplo4);
