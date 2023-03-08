3704bd67391255b7c91068e15dd00365675a989c
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 13:12:38 -0500

    feat: :memo: Operado de comparacion > (Mayor que) en strings
    El operador funciona con otros tipos de datos. En el primer el primer string es mas largo que el segundo, por lo que retorna true. En el segundo el primer string no es mas largo al segundo por lo que retorna false.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
-let x = 40;
-let y = 30;
+let x = "Hola amigos";
+let y = "Hola";
 
 let result = x > y; 
-console.log("El primer valor es mayor al segundo", result);
+console.log("El primer string es mayor al segundo", result);
 
-let ejemplo2 = 1;
-let ejemplo3 = 5;
+let ejemplo2 = "Gato";
+let ejemplo3 = "Perro";
 
 let result2 = ejemplo2 > ejemplo3;
-console.log("El segundo valor es mayor al segundo", result2);
+console.log("El segundo string es mayor al primero", result2);
