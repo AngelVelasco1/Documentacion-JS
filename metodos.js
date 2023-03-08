e3e9a86584c2bd7015b73d15b71f17253898c4d3
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 12:46:23 -0500

    feat: :sparkles: Operador de comparacion ==
    Permite comparar si dos valores son iguales, no en tipo de dato sino en contenido, retornando true. En el caso de ser diferentes sera false. "x" y "y" son iguales por lo que sera true.

--- metodos.js
+++ metodos.js
@@ -1,11 +1,11 @@
 let x = true;
-let y = false;
+let y = true;
 
-let result = x ^ y; 
+let result = x == y; 
 console.log(result);
 
 let ejemplo2 = true;
-let ejemplo3 = true;
+let ejemplo3 = false;
 
-let result2 = ejemplo2 ^ ejemplo3;
+let result2 = ejemplo2 == ejemplo3;
 console.log(result2);
