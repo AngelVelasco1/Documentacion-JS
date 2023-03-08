0391dee0b00452e5e3d100256dba0e4374e11935
AngelVelasco1 <angelgg2020@outlook.com>
2023-03-08 12:26:25 -0500

    feat: :sparkles: Operador Y
    Nos permite compara dos valores. Ambos deben ser verdaderos para que nos retorne true, en cualquier otro caso retornara false

--- metodos.js
+++ metodos.js
@@ -1,3 +1,11 @@
+let ejemplo1 = true && true;
+console.log(ejemplo1);
 
-let palabra =  "Ejemplo Metodo lastIndexOf"
-console.log(palabra.lastIndexOf("j"));
+let ejemplo2 = true && false;
+console.log(ejemplo2);
+
+let ejemplo3 = false && false;
+console.log(ejemplo3);
+
+let ejemplo4 = false && true;
+console.log(ejemplo4);
