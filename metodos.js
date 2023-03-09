724b7dbe99f395055845b3f04c06b8a34804fd23
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-03-08 21:05:20 -0500

    feat: :sparkles: Operador aritmetico residuo (%)
    Nos permite ver el residuo de dos numero. El residuo hace referencia al numero que sobra luego de efectuar una division.  En el ejemplo vemos que el residuo de 100/20 es 0, ya que no sobra nada de la division

--- metodos.js
+++ metodos.js
@@ -1,11 +1,6 @@
-let x = 20;
+let x = 100;
 let y = 20;
 
-let result = x <= y; 
-console.log("El primer valor no es menor pero si igual al segundo", result);
+let result = x % y; 
+console.log( result);
 
-let ejemplo2 = 250;
-let ejemplo3 = 20;
-
-let result2 = ejemplo2 <= ejemplo3;
-console.log("El primer valor no es menor ni igual al segundo", result2);
