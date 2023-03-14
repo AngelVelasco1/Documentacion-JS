0117d433b20d4aa90601fa01b7471d77c29b65d6
Angel Velasco <angelgg2020@outlook.com>
2023-03-14 12:31:40 -0500

    feat: :sparkles: Funcion autoejecutable
    Son funciones que se ejecutan automaticamente. Simplemente envolvemos la funcion entre parentesis, y añadimos parametros si quisieramos. En este caso esta funcion se ejecuta sin pedirselo, mostrando un mensaje en el alert.

--- metodos.js
+++ metodos.js
@@ -1,8 +1,3 @@
-const function1 = function() {
-    alert("Funcion 1 ejecutada llamandola desde la funcion 2")
-};
-
-const function2 = function(callback) {
-    callback();
-};
-function2(function1);
+(function(type) {
+    alert(`Funcion ${type}`)
+})("Autoejecutable");
